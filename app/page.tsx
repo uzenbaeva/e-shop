02a77393149debd099a208298d57965a240de2a3
uzenbaeva <akmaralzamirbekova35@gmail.com>
2024-09-16 17:03:02 +0600

    All Products UI

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import { formatCurrency } from '@/utils/formatCurrency';
 import Footer from '@/components/Footer/footer';
 import Discounts from '@/components/Discounts/discounts';
 import Countdown from '@/components/FlashSales/index';
+import ProductCard from '@/components/AllProducts/productCard';
 const Home: React.FC = () => {
   return (
     <>
       <Header />
       <Discounts />
       <Countdown />
+      <ProductCard />
       <Footer />
     </>
   );
