30567bd0e155cc514419388b0b5c06783c109785
uzenbaeva <akmaralzamirbekova35@gmail.com>
2024-09-17 14:02:54 +0600

    Conflicting files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 'use client';
-import { Product } from '../types/common';
 import Header from '@/components/Header';
-import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import { productsAPI } from '@/lib/product';
-import { formatCurrency } from '@/utils/formatCurrency';
 import Footer from '@/components/Footer/footer';
 import Discounts from '@/components/Discounts/discounts';
 import Countdown from '@/components/FlashSales/index';
